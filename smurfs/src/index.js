d69163deda48308672f53df9e5a55e83ce830ae4
Dave Betts <ibedave777@gmail.com>
2020-01-24 12:50:41 -0500

    basic functionality done... need to add styles

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+import {reducer} from './reducers/smurfReducer';
+
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger)
+);
 
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById("root")
+);
